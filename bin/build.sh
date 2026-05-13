abe872c479edf651fa3ad85a159aa11279fda90d
imxieke <oss@live.hk>
2026-05-14 01:37:46 +0800

    update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -2,11 +2,16 @@
 ###
  # @Author: Cloudflying
  # @Date: 2026-05-12 20:30:10
- # @LastEditTime: 2026-05-12 20:30:11
+ # @LastEditTime: 2026-05-14 00:59:05
  # @LastEditors: Cloudflying
  # @Description: Build Release
 ###
 
-VERSION="v0.0.1"
+VERSION="v0.0.2"
 go mod download
-go build -ldflags "-s -w -X 'new-api/common.Version=$VERSION' -extldflags '-static'" -o new-api
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -X 'new-api/common.Version=$VERSION' -extldflags '-static'" -o aiapi
+else
+  go build -ldflags "-s -w -X 'new-api/common.Version=$VERSION' -extldflags '-static'" -o aiapi
+fi
